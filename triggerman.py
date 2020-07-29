2cdb0d10a61b7cc3ce932d5c3126f6383b9034bb
Jader <jvjader@gmail.com>
2020-07-29 17:13:28 +0200

    added functionality to add button

--- triggerman.py
+++ triggerman.py
@@ -1,8 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
 
+def add_item ():
+    tk.Label (scrollable_frame, text="hi").pack ()
+
 root = tk.Tk ()
 root.title ("triggerman")
+
+# Setup scroll frame
 
 container           = ttk.Frame (root)
 canvas              = tk.Canvas (container)
@@ -19,15 +24,11 @@ scrollable_frame.bind (
 canvas.create_window ((0,0), window=scrollable_frame, anchor="nw")
 canvas.configure (yscrollcommand=scrollbar.set)
 
-
 container.pack  (fill=tk.BOTH, expand=1, anchor=tk.N)
 canvas.pack     (side=tk.LEFT, fill=tk.BOTH, expand=1)
 scrollbar.pack  (side=tk.RIGHT, fill=tk.Y)
 
-for i in range (0,50):
-    ttk.Label (scrollable_frame, text=str (i)).pack ()
-
-btn_add         = tk.Button (root, text="add")
+btn_add         = tk.Button (root, text="add", command=add_item)
 btn_remove      = tk.Button (root, text="remove", bg="red")
 btn_copy        = tk.Button (root, text="copy")
 
