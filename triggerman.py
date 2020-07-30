c180ff286eee4b1a9ac8b7a0143eafdfdea0e5f1
Jader <jvjader@gmail.com>
2020-07-30 15:16:59 +0200

    added definitions for triggers and added global trigger list

--- triggerman.py
+++ triggerman.py
@@ -1,8 +1,19 @@
 import tkinter as tk
 from tkinter import ttk
+from triggers import *
+
+def add_list_item (trigger):
+    lst_item    = tk.Frame (frame_trigger_list)
+    lbl_id      = tk.Label (lst_item, text=str (trigger.id))
+    lbl_name    = tk.Label (lst_item, text=str (trigger.name))
+
+    lst_item.pack   (fill=tk.X, expand=1, anchor=tk.NW)
+    lbl_id.pack     (side=tk.LEFT)
+    lbl_name.pack   (side=tk.LEFT)
 
 def add_item ():
-    tk.Label (scrollable_frame, text="hi").pack ()
+    add_trigger ()
+    add_list_item (trigger_list[-1])
 
 root = tk.Tk ()
 root.title ("triggerman")
@@ -12,26 +23,27 @@ root.title ("triggerman")
 container           = ttk.Frame (root)
 canvas              = tk.Canvas (container)
 scrollbar           = ttk.Scrollbar (container, orient="vertical", command=canvas.yview)
-scrollable_frame    = ttk.Frame (canvas)
+frame_trigger_list  = ttk.Frame (canvas)
 
-scrollable_frame.bind (
+frame_trigger_list.bind (
     "<Configure>",
     lambda e: canvas.configure (
         scrollregion = canvas.bbox ("all")
     )
 )
 
-canvas.create_window ((0,0), window=scrollable_frame, anchor="nw")
+canvas.create_window ((0,0), window=frame_trigger_list, anchor="nw")
 canvas.configure (yscrollcommand=scrollbar.set)
-
-container.pack  (fill=tk.BOTH, expand=1, anchor=tk.N)
-canvas.pack     (side=tk.LEFT, fill=tk.BOTH, expand=1)
-scrollbar.pack  (side=tk.RIGHT, fill=tk.Y)
 
 btn_add         = tk.Button (root, text="add", command=add_item)
 btn_remove      = tk.Button (root, text="remove", bg="red")
 btn_copy        = tk.Button (root, text="copy")
 
+# packing
+
+container.pack  (fill=tk.BOTH, expand=1, anchor=tk.N)
+canvas.pack     (side=tk.LEFT, fill=tk.BOTH, expand=1)
+scrollbar.pack  (side=tk.RIGHT, fill=tk.Y)
 btn_add.pack    (anchor=tk.NW, side=tk.LEFT, fill=tk.X)
 btn_remove.pack (anchor=tk.NW, side=tk.LEFT, fill=tk.X)
 btn_copy.pack   (anchor=tk.NW, side=tk.LEFT, fill=tk.X)
---
+++ triggers.py
@@ -0,0 +1,28 @@
+import collections
+
+# define list tuple
+trigger_t = collections.namedtuple (
+    "trigger_t",
+    [
+        'id',
+        'name',
+        'path',
+        'enabled',
+        'activation_frame'
+    ])
+
+# globals
+trigger_list = []
+
+# function definitions
+def add_trigger ():
+    _id = len (trigger_list)
+    trigger = trigger_t (
+        id                  =  _id,
+        name                = "trigger_%i" % _id,
+        path                = "No file set",
+        enabled             = 0,
+        activation_frame    = 0
+    )
+
+    trigger_list.append (trigger)
