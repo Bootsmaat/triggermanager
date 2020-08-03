216b047cfb2d082ccae86575da195e76cca35f66
Jader <jvjader@gmail.com>
2020-08-03 17:51:56 +0200

    added feedback to entering data

--- triggerman.py
+++ triggerman.py
@@ -1,16 +1,20 @@
 import tkinter as tk
 from tkinter import ttk, filedialog, IntVar
 from triggers import *
+from os import path
 
 root = tk.Tk ()
 root.title ("triggerman")
 root.geometry ("650x600")
 
+# gui globals
+FLASH_LENGTH = 200
+
 # globals
 selected_item = IntVar()
 trigger_item_list = []
 
-def open_file (id):
+def open_file ():
     _path = filedialog.askopenfilename (
         filetypes=[
             ("all files", "*.*"),
@@ -18,8 +22,38 @@ def open_file (id):
             ("WAV files", "*.wav")
         ]
     )
+    return _path
 
-    update_trigger (id, path=_path)
+def update_element (_item, **kwargs):
+    for key in kwargs:
+        _item[key] = kwargs[key]
+
+def update_trigger_wrapper (_item, _id, **kwargs):
+
+    orig_color = _item.cget ("background")
+
+    if 'path' in kwargs:
+        _path = open_file ()
+        if not (len (_path) == 0):
+            kwargs['path'] = _path
+            _item['text'] = _path.split(path.sep)[-1]
+
+    if 'activation_frame' in kwargs:
+        try:
+            kwargs['activation_frame'] = int (kwargs['activation_frame'])
+        except ValueError:
+            _item['bg'] = "red"
+            _item.delete (0, tk.END)
+        else:
+            _item['bg'] = "green"
+            update_trigger (id=_id, **kwargs)
+        finally:
+            _item.after (FLASH_LENGTH, lambda: update_element (_item, bg=orig_color))
+    else:
+        _item['bg'] = "green"
+        update_trigger (id=_id, **kwargs)
+        _item.after (FLASH_LENGTH, lambda: update_element (_item, bg=orig_color))
+
 
 def add_list_item (trigger):
     global selected_item
@@ -30,8 +64,15 @@ def add_list_item (trigger):
     btn_selected    = tk.Radiobutton    (lst_item, variable=selected_item, value=trigger.id)
     lbl_id          = tk.Label          (lst_item, text="%02i" % trigger.id)
     entry_name      = tk.Entry          (lst_item)
-    btn_filepath    = tk.Button         (lst_item, text="select file", 
-                                            command=lambda: open_file (trigger.id))
+    btn_filepath    = tk.Button         (
+        lst_item,
+        text="select file",
+        command=lambda: update_trigger_wrapper (
+            btn_filepath,
+            trigger.id,
+            path=1
+        )
+    )
     lbl_enabled     = tk.Label          (lst_item, text="enabled:")
     btn_enabled     = tk.Checkbutton    (lst_item, variable=trigger.enabled)
     entry_tframe    = tk.Entry          (lst_item)
@@ -39,13 +80,21 @@ def add_list_item (trigger):
     entry_name.insert   (0, trigger.name)
     entry_name.bind     (
         "<Return>",
-        lambda a: update_trigger (id=trigger.id, name=entry_name.get ())
+        lambda a: update_trigger_wrapper (
+            _item=entry_name,
+            _id=trigger.id,
+            name=entry_name.get ()
+            )
         )
 
     entry_tframe.insert (0, str (trigger.activation_frame))
     entry_tframe.bind   (
         "<Return>",
-        lambda a: update_trigger (id=trigger.id, activation_frame=entry_tframe.get ())
+        lambda a: update_trigger_wrapper (
+            _item=entry_tframe,
+            _id=trigger.id,
+            activation_frame=entry_tframe.get ()
+            )
     )
 
     lst_item.pack       (fill=tk.X, expand=1, anchor=tk.NW)
