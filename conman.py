5df3a7ccf72df37201b135ab51dcb361be2f2244
Jader <jvjader@gmail.com>
2020-08-17 14:05:32 +0200

    testing success

--- conman.py
+++ conman.py
@@ -1,12 +1,21 @@
-import socket, sys, threading
+import socket, sys, threading, struct
 from triggers import trigger_t
+
+#define opcodes here
+OP_START    = 0x1
+OP_STOP     = 0x2
+OP_EXIT     = 0x3
+OP_GET      = 0x4
+OP_SET      = 0x5
+OP_TSET     = 0x6
+OP_TSTART   = 0x7
+OP_TSTOP    = 0x8
 
 callbacks   = []
 conf_sock   = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
 tr_sock     = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
 conf_sock.settimeout    (100)
 tr_sock.settimeout      (100)
-
 
 def trigger_loop ():
     global callbacks
@@ -34,6 +43,29 @@ TR_PORT     = 8082
 BUF_SIZE    = 32
 thr_loop    = threading.Thread (name='thr_loop', target=trigger_loop)
 thr_looping = 0
+
+def construct_packet (opc, **kwargs):
+    packstr = "B B "
+    plen = 2
+    vals = (plen, opc)
+
+    if 'cat_set' in kwargs:
+        slen = len(kwargs['cat_set'])
+        plen += slen
+        packstr += slen + "s "
+        vals = (*vals, kwargs['cat_set'])
+
+    if 'tr_id' in kwargs:
+        if not 'tr_afr' in kwargs:
+            print ("conman: incomplete arguments. tr_afr missing")
+            return
+        plen += 4       
+        packstr += "H H "
+        vals = (*vals, kwargs['tr_id'], kwargs['tr_afr'])
+
+    vals[0] = plen
+    packer = struct.Struct (packstr)
+    return packer.pack (*vals)
 
 #function definitions
 def connect (addr):
